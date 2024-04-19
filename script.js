39342e999e8de90778765e39380c1c19589a75a1
Sermad Ahmad <71033@gcslahore.edu.pk>
2024-04-19 22:43:50 +0500

    improved a little bit

--- script.js
+++ script.js
@@ -1,40 +1,22 @@
+const timeOut = 10000;
+let timeOutId = undefined;
 document.addEventListener('DOMContentLoaded', () => {
     const calcBtn = document.querySelector('.cal');
     calcBtn.addEventListener('click',(event) => {
+        clearTimeout(timeOutId);
         event.preventDefault();
         const num1 = document.querySelector('#num1').value;
         const num2 = document.querySelector('#num2').value;
         const op = document.querySelector('#op').value;
-        const res = calculate(parseFloat(num1), parseFloat(num2), op);
-        document.querySelector('#result').innerText = "Result: " + res;
-        setTimeout( () => {
+        // const res = calculate(parseFloat(num1), parseFloat(num2), op);
+        // const exp = "";
+        const res = eval(`${num1} ${op} ${num2}`);
+        if (res === undefined)
+            document.querySelector('#result').innerText = "Error Error";
+        else 
+            document.querySelector('#result').innerText = "Result: " + res;
+        timeOutId = setTimeout( () => {
             document.querySelector('#result').innerText = "";
-        }, 10000);
+        }, timeOut);
     });
 });
-
-const calculate = (num1, num2, op) => {
-    let res = 0;
-    switch (op) {
-        case '+':
-            res = num1 + num2;
-            console.log(typeof num1);
-            break;
-        case '-':
-            res = num1 - num2;
-            console.log(typeof num1);
-            break;
-        case '*':
-            res = num1 * num2;
-            break;
-        case '÷':
-            res = num1 / num2;
-            break;
-        case '%':
-            res = num1 % num2;
-            break;
-        default:
-            res = 'Error Error';        
-    }
-    return res;
-   };
